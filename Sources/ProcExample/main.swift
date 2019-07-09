1e4370ec9d5a3312f9c05f44f1b3c6757e3ca339
Nikita Starshinov <nstarshinov@yandex.ru>
2019-07-10 01:07:10 +0300

    Update ProcExample

--- Sources/ProcExample/main.swift
+++ Sources/ProcExample/main.swift
@@ -4,12 +4,13 @@ import Proc
 let ls = Proc("/usr/bin/env", "find", ".", "-type", "f")
 let sort = Proc("/usr/bin/sort", "-rh")
 let wc = Proc("/usr/bin/xargs", "wc", "-c")
-let ffmpeg = Proc(name: "ffmpeg", "-v")
+let ffmpeg = Proc(name: "ffmpeg", "-version")
 
 do {
     let p = ls
         .pipe(to: wc)
-        .pipe(to: sort)    
+        .pipe(to: sort)
+//    let p = ffmpeg
     print(p)
     let output = try p.runForStdout()
     print("OUTPUT START\n\(output)\nOUTPUT END")
